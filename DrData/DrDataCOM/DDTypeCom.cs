23577aedb19401676193f2e70961d2b6dc5abc8d
dropen <kudryashov.andrey@gmail.com>
2017-10-07 12:38:45 +0300

    [+] added com support first iteration

---
+++ DrData/DrDataCOM/DDTypeCom.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrCommon.DrDataCom
+{
+    [Guid("E905F26E-212E-4b16-8CED-DDD54B60BD36")]
+    public interface IDDTypeCom // cannot list any base interfaces here 
+    {
+        // Note that the members of IUnknown and Interface are NOT
+        // listed here 
+        //
+        [DispId(1)]
+        string Name {get; set; }
+
+
+        [DispId(2)]
+        void ValidateExpectedNodeType([In, MarshalAs(UnmanagedType.BStr)]  string expectedType);
+        
+
+        //void Stop();
+
+        //void GetState([In] int msTimeout, [Out] out int pfs);
+
+        //void RenderFile(
+        //[In, MarshalAs(UnmanagedType.BStr)] string strFilename);
+
+
+        //void AddSourceFilter(
+        //[In, MarshalAs(UnmanagedType.BStr)] string strFilename,
+        //[Out, MarshalAs(UnmanagedType.Interface)] out object ppUnk);
+
+        //[return: MarshalAs(UnmanagedType.Interface)]
+        //object FilterCollection();
+
+        //[return: MarshalAs(UnmanagedType.Interface)]
+        //object RegFilterCollection();
+
+        //void StopWhenReady();
+    }
+
+    [Guid("D5D3F13D-E071-4437-8613-16D6D5927589")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IDDTypeComEvents
+    {
+    }
+
+    [Guid("FA8372F0-8981-451d-848A-6FB802853D5C")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(IDDTypeComEvents))]
+    [ProgId("DrDataCom.IDDTypeCom")]
+    public class DDTypeCom : DDType, IDDTypeCom
+    {
+
+        public DDTypeCom () : base (String.Empty)
+        { }
+        ~DDTypeCom() { }
+
+        public void ValidateExpectedNodeType(string expectedType)
+        {
+            base.ValidateExpectedNodeType(expectedType);
+            //if ((expectedType == null) || (expectedType.Length==0)) return; // if array is empty exit
+            //string[] a;
+            //a = new string[expectedType.Length];
+            //for (int i = 0; i < expectedType.Length; i++)
+            //{
+            //    a[i] = expectedType[i].ToString();
+            //}
+        }
+
+    }
+}
---
+++ DrData/DrDataCOM/DDValueCom.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrCommon.DrDataCom
+{
+        [Guid("1335CCD7-EE3D-4d84-BBB7-65CC800C3084")]
+    public interface IDDValueCom // cannot list any base interfaces here 
+    {
+        // Note that the members of IUnknown and Interface are NOT
+        // listed here 
+        //
+        [DispId(1)]
+            Type Type { [return: MarshalAs(UnmanagedType.)] get; }
+       // [DispId(2)]
+
+        //long SizeValue { [return: MarshalAs(UnmanagedType.I8)] get; }
+             
+        //[DispId(2)]
+        //void ValidateExpectedNodeType([In, MarshalAs(UnmanagedType.BStr)]  string expectedType);
+        
+
+        //void Stop();
+
+        //void GetState([In] int msTimeout, [Out] out int pfs);
+
+        //void RenderFile(
+        //[In, MarshalAs(UnmanagedType.BStr)] string strFilename);
+
+
+        //void AddSourceFilter(
+        //[In, MarshalAs(UnmanagedType.BStr)] string strFilename,
+        //[Out, MarshalAs(UnmanagedType.Interface)] out object ppUnk);
+
+        //[return: MarshalAs(UnmanagedType.Interface)]
+        //object FilterCollection();
+
+        //[return: MarshalAs(UnmanagedType.Interface)]
+        //object RegFilterCollection();
+
+        //void StopWhenReady();
+    }
+
+    [Guid("EC39B55B-AE93-4aff-ACFC-9A0BD265A0D0")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIDispatch)]
+    public interface IDDValueComEvents
+    {
+    }
+
+    [Guid("8FD62AE4-924F-45a0-9D25-88F2FB95F324")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComSourceInterfaces(typeof(IDDValueComEvents))]
+    [ProgId("DrDataCom.IDDValueCom")]
+    public class DDValueCom : DDValue, IDDValueCom
+    {
+        public long SizeValue { get { return base.Size; } }
+        public Type Type { get { return typeof(string); } }
+
+    }
+}
