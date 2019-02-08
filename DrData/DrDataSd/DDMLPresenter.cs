d69f5e2c87a5266fafac8a2da1df3a574e618f3c
nzabelina <47331640+nzabelina@users.noreply.github.com>
2019-02-08 15:53:30 +0300

    DDNode to DDML serialization
    (add) DDNode to DDML serialization

---
+++ DrData/DrDataSd/DDMLPresenter.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DrOpen.DrCommon.DrData;
+
+namespace DrOpen.DrData.DrDataSd
+{
+    /// <summary>
+    /// Presents DDNode as DDML string    /// </summary>
+    public static class DDMLPresenter
+    {
+        #region Tags
+        private const string nodeTag = ": ";
+        private const string attrTag = " = ";
+        private const string separator = "  ";
+        #endregion
+
+        public static string DDnodeToDDML(DDNode n)
+        {
+            var result = new StringBuilder();
+
+            if (n.Level > 1) result.Append(new StringBuilder().Insert(0, separator, (int)n.Level).ToString()); //node formatting 
+
+            result.AppendLine(ProcessNodeNameAndType(n));
+
+            if (n.HasAttributes)
+            {
+                result.Append(ProcessAttributes(n.Attributes, new StringBuilder().Insert(0, separator, (int)n.Level + 1).ToString()));
+            }
+
+            if (n.HasChildNodes)
+            {
+                foreach (var child in n.Values)
+                {
+                    if (child.Type.Name.Contains("Schema")) //  Skips node if node type is Schema
+                    {
+                        child.Skip(1);
+                    }
+                    else
+                    {
+                        result.Append(DDnodeToDDML(child));
+                    }
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static string ProcessAttributes(DDAttributesCollection ac, string level)
+        {
+            var result = new StringBuilder();
+
+            foreach (var attr in ac)
+            {
+                string value = attr.Value.ToString();
+                string name = attr.Key.ToString();
+                Type type = attr.Value.Type;
+
+                result.Append(level);
+
+                if (type.IsPrimitive || type == typeof(System.Guid) || type == typeof(System.DateTime))
+                {
+                    result.AppendLine(PrimitiveTypeAttribute(name, value));
+                }
+
+                if (type == typeof(System.String))
+                {
+                    result.AppendLine(StringTypeAttribute(name, value));
+                }
+
+                if (type.IsArray)
+                {
+                    result.AppendLine(ArrayTypeAttribute(name, attr.Value));
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static string ProcessNodeNameAndType(DDNode n)
+        {
+            string type = n.Type.Name;
+            string name = n.Name;
+            return String.Concat(type, nodeTag, name);
+        }
+
+        private static string PrimitiveTypeAttribute(string name, string value)
+        {
+            return String.Concat(name, attrTag, value);
+        }
+
+        private static string StringTypeAttribute(string name, string value)
+        {
+            return String.Concat(name, attrTag, "\"", value, "\"");
+        }
+
+        private static string ArrayTypeAttribute(string name, DDValue value)
+        {
+            string[] resultStringArray = value.ToStringArray();
+            string resultString = String.Join(", ", resultStringArray);
+
+            if (resultStringArray.Length == 1)
+            {
+                return String.Concat(name, attrTag, resultString, ", ");
+            }
+            else
+            {
+                return String.Concat(name, attrTag, resultString);
+            }
+        }
+    }
+}
---
+++ DrData/DrDataSd/DDNodeSd.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DrOpen.DrCommon.DrData;
+using DrOpen.DrData.DrDataSd;
+
+namespace DrOpen.DrCommon.DrDataSd
+{
+    /// <summary>
+    /// converts DDNode to DDML 
+    /// </summary>
+    public class DDNodeSd
+    {
+        private DDNode n;
+
+        public DDNodeSd(DDNode n)
+        {
+            this.n = n;
+        }
+
+        public DDNode GetValue()
+        {
+            return this.n;
+        }
+
+        /// <summary>
+        /// get DDNode representation as DDML string builder
+        /// </summary>
+        /// <param name="sb"></param>
+        public void WriteDDML(StringBuilder sb)
+        {
+            DDNodeSde.Serialize(this.n, sb);
+        }
+        /// <summary>
+        /// get DDNode representation as DDML stream
+        /// </summary>
+        /// <param name="st"></param>
+        public void WriteDDML(Stream st)
+        {
+            DDNodeSde.Serialize(this.n, st);
+        }
+    }
+
+    public static class DDNodeSde
+    {
+        /// <summary>
+        /// Provides serialization DDNode to DDML StringBuilder
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="sb"></param>
+        public static void Serialize(DDNode n, StringBuilder sb)
+        {
+            using (StringWriter sw = new StringWriter(sb))
+            {
+                sw.Write(DDMLPresenter.DDnodeToDDML(n));
+            }
+        }
+
+        /// <summary>
+        /// Provides serialization DDNode to DDML stream
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="s"></param>
+        public static void Serialize(DDNode n, Stream s)
+        {
+            using (StreamWriter sw = new StreamWriter(s))
+            {
+                sw.Write(DDMLPresenter.DDnodeToDDML(n));
+            }
+        }
+    }
+
+    /// <summary>
+    /// extends DDNode to call SerializeToDDML as a static method of DDNode instance
+    /// </summary>
+    public static class DDNodeExtention
+    {
+        public static void SerializeToDDML(this DDNode n, StringBuilder sb)
+        {
+            DDNodeSde.Serialize(n, sb);
+        }
+
+        public static void SerializeToDDML(this DDNode n, Stream st)
+        {
+            DDNodeSde.Serialize(n, st);
+        }
+    }
+}
